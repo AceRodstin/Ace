31febb87af1a375bae2fb578ee5c21780323ecf0
Ace Rodstin <acerodstin@gmail.com>
2023-02-10 07:13:47 +0300

    Implement main.siwft

--- Ace/main.swift
+++ Ace/main.swift
@@ -8,4 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+private func sourceCodePath() -> String? {
+	let arguments = CommandLine.arguments
+	let sourceFlagIndex = arguments.firstIndex { $0 == "--source" }
+	
+	if let sourceFlagIndex {
+		let filePathIndex = arguments.index(after: sourceFlagIndex)
+		return arguments[filePathIndex]
+	} else {
+		return nil
+	}
+}
+
+private func compile(file path: String) {
+	let compiler = Compiler()
+
+	do {
+		let records = try compiler.compile(file: path)
+		print(records)
+	} catch {
+		print("Compilation error")
+	}
+}
+
+if let path = sourceCodePath() {
+	compile(file: path)
+} else {
+	print("Specify source code path following \"--source\" argument.")
+}
