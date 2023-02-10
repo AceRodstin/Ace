794d500c51eb85ac2770dadb38f02849bb2283d8
Ace Rodstin <acerodstin@gmail.com>
2023-02-10 07:07:50 +0300

    Project created.

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -1,12 +1,13 @@
 //
-//  main.swift
-//  Ace
+//  Tests.swift
+//  Tests
 //
 //  Created by Ace Rodstin on 2/10/23.
 //  Copyright © 2023 Ace Rodstin. All rights reserved.
 //
 
-import Foundation
+import XCTest
 
-print("Hello, World!")
+final class Tests: XCTestCase {
 
+}
